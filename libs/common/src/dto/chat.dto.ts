5d7cd524a9b83a8ae432be708a30692b4c797f4c
Adam Rafiandri <rafiandria23@gmail.com>
2024-01-24 12:50:02 +0700

    docs(chat): add `@ApiProperty()` decorators in DTOs

--- libs/common/src/dto/chat.dto.ts
+++ libs/common/src/dto/chat.dto.ts
@@ -1,22 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsMongoId } from 'class-validator';
 
 export class ChatCreateRoomDto {
+  @ApiProperty({
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   readonly name: string;
 }
 
 export class ChatJoinRoomDto {
+  @ApiProperty({
+    required: true,
+  })
   @IsMongoId()
   @IsNotEmpty()
   readonly room_id: string;
 }
 
 export class ChatSaveMessageDto {
+  @ApiProperty({
+    required: true,
+  })
   @IsMongoId()
   @IsNotEmpty()
   readonly room_id: string;
 
+  @ApiProperty({
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   readonly content: string;
