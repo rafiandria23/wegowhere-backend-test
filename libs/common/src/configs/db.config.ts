777e4ad9d3759f52c2864ba9a0242c569f0046ce
Adam Rafiandri <rafiandria23@gmail.com>
2024-05-11 12:28:59 +0700

    chore: use `localhost` for default host

--- libs/common/src/configs/db.config.ts
+++ libs/common/src/configs/db.config.ts
@@ -1,9 +1,10 @@
+import _ from 'lodash';
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('db', () => ({
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: parseInt(process.env.DB_PORT, 10) || 27017,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
-  name: process.env.DB_NAME || 'wegowhere',
+  host: _.defaultTo(process.env.DB_HOST, 'localhost'),
+  port: _.defaultTo(parseInt(process.env.DB_PORT, 10), 27017),
+  user: _.defaultTo(process.env.DB_USER, 'wegowhere'),
+  pass: _.defaultTo(process.env.DB_PASS, 'wegowhere'),
+  name: _.defaultTo(process.env.DB_NAME, 'wegowhere'),
 }));
--- libs/common/src/configs/gateway.config.ts
+++ libs/common/src/configs/gateway.config.ts
@@ -1,6 +1,7 @@
+import _ from 'lodash';
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('gateway', () => ({
-  host: process.env.GATEWAY_HOST || '0.0.0.0',
-  port: parseInt(process.env.GATEWAY_PORT, 10) || 3000,
+  host: _.defaultTo(process.env.GATEWAY_HOST, 'localhost'),
+  port: _.defaultTo(parseInt(process.env.GATEWAY_PORT, 10), 3000),
 }));
